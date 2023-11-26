1f774f3bef3e5c954ca01450d48f7bc3df8c1790
Franklin-tech01 <franklinmike2021@gmail.com>
2023-11-26 13:49:20 +0000

    api warrs

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 const request = require('request');
 
 const movieId = process.argv[2];
@@ -29,20 +30,30 @@ request(apiUrl, (error, response, body) => {
     process.exit(1);
   }
 
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-        process.exit(1);
-      }
-
-      if (response.statusCode !== 200) {
-        console.error('Invalid response:', response.statusCode);
-        process.exit(1);
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
+  const fetchCharacterData = (characterUrl) => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else if (response.statusCode !== 200) {
+          reject(new Error(`Invalid response for character data: ${response.statusCode}`));
+        } else {
+          const characterData = JSON.parse(body);
+          resolve(characterData.name);
+        }
+      });
     });
-  });
+  };
+
+  // Use Promise.all to fetch all character data concurrently
+  Promise.all(characters.map(fetchCharacterData))
+    .then(characterNames => {
+      characterNames.forEach(name => {
+        console.log(name);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      process.exit(1);
+    });
 });
